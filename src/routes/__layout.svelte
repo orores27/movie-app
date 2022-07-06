b11b62e8a3ac325051770e85cafa8d22d145a5d5
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-07-06 16:14:44 +0200

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import global from '../global.css';
+    import Nav from '../components/Nav.svelte';
+</script>
+
+<Nav />
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+    export async function load({fetch}) {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d87d9bd9cbb63454314c8992f55cab0c&language=fr-FR`);
+        const data = await response.json();
+        if(response.ok) {
+            return {
+                props: {popular: data.results}
+            };
+        };
+    };
+</script>
+
+<script>
+    import PopularMovies from "../components/PopularMovies.svelte";
+    import SearchMovies from "../components/SearchMovies.svelte";
+    export let popular;
+</script>
+
+<section>
+    <SearchMovies />
+    <PopularMovies {popular} />
+</section>
